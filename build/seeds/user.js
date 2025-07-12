ceb530edd769a419d291a1b3f8c863a5e2f717f4
MasRama <ryushineo@gmail.com>
2025-07-12 13:31:53 +0700

    add user

---
+++ build/seeds/user.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.seed = seed;
+const uuid_1 = require("uuid");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+async function seed(knex) {
+    await knex("users").del();
+    const hashedPassword = await bcrypt_1.default.hash('123', 10);
+    const now = Date.now();
+    await knex("users").insert([
+        {
+            id: (0, uuid_1.v4)(),
+            name: 'Test User',
+            email: 'tes@gmail.com',
+            phone: null,
+            is_verified: false,
+            membership_date: null,
+            is_admin: false,
+            password: hashedPassword,
+            remember_me_token: null,
+            created_at: now,
+            updated_at: now
+        }
+    ]);
+}
+;
+//# sourceMappingURL=user.js.map
---
+++ seeds/user.ts
@@ -0,0 +1,29 @@
+import { Knex } from "knex";
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("users").del();
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash('123', 10);
+    const now = Date.now();
+
+    // Inserts seed entries
+    await knex("users").insert([
+        {
+            id: uuidv4(),
+            name: 'Test User',
+            email: 'tes@gmail.com',
+            phone: null,
+            is_verified: false,
+            membership_date: null,
+            is_admin: false,
+            password: hashedPassword,
+            remember_me_token: null,
+            created_at: now,
+            updated_at: now
+        }
+    ]);
+};
